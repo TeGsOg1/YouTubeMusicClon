c51c63fa73c1a4ff648d1f06a3c6378492717929
TeGsOg1 <tomasyluti@gmail.com>
2024-05-23 20:07:58 -0300

    update astro and add volume controls

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { usePlayerStore } from "@/store/PlayerStore";
 import { useEffect, useRef } from "react";
-
+import { Slider } from "./Slider";
 
 export const Pause = () => (
   <svg
@@ -49,49 +49,101 @@ export const Play = () => (
   </svg>
 );
 
-const CurrentSong = ({ image, title }) => {
-  
-  return(
-    <section className="flex flex-row items-center gap-4 pt-2 max-w-52">
-      <picture className="w-16 h-16 bg-slate-500 ">
-        <img src={image} className="h-16 w-20" alt={ title } />
-      </picture>
-      <h3 className=" text-ellipsis">
-        {title}
-      </h3>
-    </section>
-  )
+export const VolumeMute = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="#ffffff"
+      d="m19.8 22.6l-3.025-3.025q-.625.4-1.325.688t-1.45.462v-2.05q.35-.125.688-.25t.637-.3L12 14.8V20l-5-5H3V9h3.2L1.4 4.2l1.4-1.4l18.4 18.4zm-.2-5.8l-1.45-1.45q.425-.775.638-1.625t.212-1.75q0-2.35-1.375-4.2T14 5.275v-2.05q3.1.7 5.05 3.138T21 11.975q0 1.325-.363 2.55T19.6 16.8m-3.35-3.35L14 11.2V7.95q1.175.55 1.838 1.65T16.5 12q0 .375-.062.738t-.188.712M12 9.2L9.4 6.6L12 4z"
+    />
+  </svg>
+);
 
-}
+export const Volume = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="#fff"
+      d="M14 20.725v-2.05q2.25-.65 3.625-2.5t1.375-4.2t-1.375-4.2T14 5.275v-2.05q3.1.7 5.05 3.138T21 11.975t-1.95 5.613T14 20.725M3 15V9h4l5-5v16l-5-5zm11 1V7.95q1.175.55 1.838 1.65T16.5 12q0 1.275-.663 2.363T14 16"
+    />
+  </svg>
+);
+const CurrentSong = ({ image, title }) => {
+  return (
+    <section className="flex flex-row items-center gap-4 pt-2 w-[300px]">
+      <div className="min-w-16 min-h-16 bg-slate-500 ">
+        <img src={image} className="h-16 w-16" alt={title} />
+      </div>
+      <span className="truncate">{title}</span>
+    </section>
+  );
+};
+
+const VolumeControls = () => {
+  const volume = usePlayerStore((state) => state.volume);
+  const setVolume = usePlayerStore((state) => state.setVolume);
+
+  return (
+    <div className="flex justify-center justify-items-center  text-white gap-3">
+        <button className=" cursor-default">
+          {volume < 0.1 ? <VolumeMute /> : <Volume />}
+        </button>
+      <Slider
+        defaultValue={[100]}
+        min={0}
+        max={100}
+        className="w-24"
+        onValueChange={(value) => {
+          const [newVolume] = value;
+          const volumeValue = newVolume / 100;
+          setVolume(volumeValue);
+        }}
+      />
+    </div>
+  );
+};
 
 export function Player() {
-  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state)
-  const audioRef = useRef()
+  const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(
+    (state) => state
+  );
+  const audioRef = useRef();
+
 
   useEffect(() => {
-    isPlaying
-      ? audioRef.current.play()
-      : audioRef.current.pause()
-  }, [isPlaying])
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
 
   useEffect(() => {
-    const { song, playlist } = currentMusic
+    const { song } = currentMusic;
     if (song) {
-      const src = `/music/${song.id}/${song.id}.mp3`
-      audioRef.current.src = src
-      audioRef.current.play()
+      const src = `/music/${song.id}/${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.volume = volume;
+      audioRef.current.play();
     }
-  }, [currentMusic])
+  }, [currentMusic]);
+
+  useEffect(() => {
+    audioRef.current.volume = volume;
+  }, [volume]);
 
   const handleClick = () => {
-    setIsPlaying(!isPlaying)
-  }
-
+    setIsPlaying(!isPlaying);
+  };
 
   return (
     <div className="flex flex-row w-full px-4 justify-between z-50 text-white">
-      <div className="hidden md:block md:w-[200px]"> 
-      <CurrentSong {...currentMusic.song} />
+      <div className="hidden md:block ">
+        <CurrentSong {...currentMusic.song} />
       </div>
       <div className="grid place-content-center ">
         <div className="flex flex-row justify-center items-center">
@@ -102,7 +154,9 @@ export function Player() {
         </div>
       </div>
 
-      <div className="md:grid hidden place-content-center">VOLUME</div>
+      <div className="flex ">
+        <VolumeControls />
+      </div>
     </div>
   );
 }
--- src/store/PlayerStore.js
+++ src/store/PlayerStore.js
@@ -2,6 +2,8 @@ import { create } from "zustand";
 
 export const usePlayerStore = create((set) => ({
     isPlaying: false,
+    volume: 1,
+    setVolume: (volume) => set({ volume }),
     currentMusic: { playlist: null, song: null, songs: [] },
     setIsPlaying: (isPlaying) => set({ isPlaying }),
     setCurrentMusic: (currentMusic) => set({ currentMusic }),
