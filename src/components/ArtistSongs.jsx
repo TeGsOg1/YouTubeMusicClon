1c8ab1f75b91390dacd9b1d4d60bc28032a1a28a
TeGsOg1 <tomasyluti@gmail.com>
2024-10-02 00:46:57 -0300

    fix size of image songs

--- src/components/ArtistSongs.jsx
+++ src/components/ArtistSongs.jsx
@@ -14,9 +14,7 @@ export const ArtistSongs = ({ id }) => {
                             <CardPlayButton padding="p-3" id={song.id} />
                         <img src={song.image} className=" size-14" alt={song.title} />
                     </div>
-                    <div className=" w-96 justify-between flex">
                         <h1 className="text-white text-xl" >{song.title}</h1>
-                    </div>
                 </li>
             ))}
         </ul>
